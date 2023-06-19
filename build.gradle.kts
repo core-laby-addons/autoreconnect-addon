8475c13fb03c4f4e1a1224dff33a7e6cedbe1e10
Nico <jumpingpxl@gmail.com>
2023-06-20 00:15:44 +0200

    bump labygradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,8 @@ labyMod {
                 "1.18.2",
                 "1.19.2",
                 "1.19.3",
-                "23w03a"
+                "1.19.3",
+                "1.20.1"
         ) { version, provider ->
             configureRun(provider, version)
         }
@@ -70,14 +71,6 @@ fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionPro
     }
 
     provider.javaVersion = when (gameVersion) {
-        "1.8.9", "1.12.2", "1.16.5" -> {
-            JavaVersion.VERSION_1_8
-        }
-
-        "1.17.1" -> {
-            JavaVersion.VERSION_16
-        }
-
         else -> {
             JavaVersion.VERSION_17
         }
